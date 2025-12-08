06d853bfa5916e5eccc8ad18b044fa015a141482
budpoetra <budpoetra@gmail.com>
2025-12-08 14:20:01 +0700

    dev-documentation: enable swagger ui

--- src/main/java/com/juaracoding/sikas/controller/GlobalFallbackController.java
+++ src/main/java/com/juaracoding/sikas/controller/GlobalFallbackController.java
@@ -10,33 +10,33 @@ Created on 11/16/2025 11:00 AM
 Version 1.0
 */
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Controller
-public class GlobalFallbackController {
-
-    @RequestMapping(value = "/**", method = {
-            RequestMethod.GET,
-            RequestMethod.POST,
-            RequestMethod.PUT,
-            RequestMethod.DELETE,
-            RequestMethod.PATCH,
-            RequestMethod.OPTIONS
-    })
-    public ResponseEntity<Object> fallback() {
-        Map<String, Object> response = new HashMap<>();
-        response.put("success", false);
-        response.put("message", "The requested resource was not found.");
-        response.put("status", HttpStatus.NOT_FOUND.value());
-        response.put("data", null);
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-    }
-}
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Controller
+//public class GlobalFallbackController {
+//
+//    @RequestMapping(value = "/**", method = {
+//            RequestMethod.GET,
+//            RequestMethod.POST,
+//            RequestMethod.PUT,
+//            RequestMethod.DELETE,
+//            RequestMethod.PATCH,
+//            RequestMethod.OPTIONS
+//    })
+//    public ResponseEntity<Object> fallback() {
+//        Map<String, Object> response = new HashMap<>();
+//        response.put("success", false);
+//        response.put("message", "The requested resource was not found.");
+//        response.put("status", HttpStatus.NOT_FOUND.value());
+//        response.put("data", null);
+//
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+//    }
+//}
